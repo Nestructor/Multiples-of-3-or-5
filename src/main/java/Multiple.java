89c0b8584690f69351331a3a84514abc413045a5
Nestor <nestor.bolanos102@alu.ulpgc.es>
2020-01-22 13:12:52 +0000

    First Test: negative number

---
+++ src/main/java/Multiple.java
@@ -0,0 +1,7 @@
+public class Multiple {
+
+    public int of(int number) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/Multiple_.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Multiple_ {
+
+    private Multiple multiple;
+
+    @Before
+    public void setUp() {
+        multiple = new Multiple();
+    }
+
+    @Test
+    public void given_negative_number_should_return_0() {
+        assertThat(multiple.of(-1)).isEqualTo(0);
+    }
+
+}
